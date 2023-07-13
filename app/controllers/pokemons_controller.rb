9c0f8f2ad21aa55f7816cc1fadda198d9188388d
JadFu <84548552+JadFu@users.noreply.github.com>
2023-07-12 21:54:55 -0500

    enable edit

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -5,4 +5,27 @@ class PokemonsController < ApplicationController
     def show
       @pokemon = Pokemon.find(params[:id])
     end
+
+    def edit
+      @pokemon = Pokemon.find(params[:id])
+    end
+  
+    def update
+      @pokemon = Pokemon.find(params[:id])
+    
+      if @pokemon.update(pokemon_params)
+        redirect_to @pokemon, notice: "Pokemon was successfully updated."
+      else
+        flash.now[:alert] = "Failed to update the Pokemon."
+        render :edit
+      end
+    end
+    
+    
+
+    private
+
+    def pokemon_params
+      params.require(:pokemon).permit(:name, :HP, :ATK, :DEF, :SA, :SD, :SP)
+    end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
     resources :pokemons, only: [:index]
-    resources :pokemons, only: [:index, :show]
+    resources :pokemons, only: [:index, :show, :edit, :update, :delete]
 end
