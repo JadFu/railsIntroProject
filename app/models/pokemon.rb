7da1fc8b6b2905f98f6ca8cfa3044ec25a07d6c3
JadFu <84548552+JadFu@users.noreply.github.com>
2023-07-13 12:01:12 -0500

    add bootstrap css for top nav bar

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,4 +1,5 @@
 class Pokemon < ApplicationRecord
     belongs_to :type_id, class_name: 'Type', foreign_key: "type1"
     belongs_to :type_id, class_name: 'Type', foreign_key: "type2", optional: true
+    validates :name, :type1, :HP, :ATK, :DEF, :SA, :SD, :SP, presence: true
 end
